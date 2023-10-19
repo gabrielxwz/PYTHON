7e8a23d500b34f52edf4e66f102f5e983e8d3e32
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-10-19 15:15:34 -0300

    atualizando f_strings.py

---
+++ aula10.19/f_strings.py
@@ -0,0 +1,8 @@
+# Interpolando strings com cogigo python
+nome = input("qual seu nome: ")
+ 
+ # Concatenação
+print("seja muito bem-vindo ", nome)
+
+# Interpolação
+print(f'seja muito bem-vindo {nome}, estavamos esperando por voce~!!!')
