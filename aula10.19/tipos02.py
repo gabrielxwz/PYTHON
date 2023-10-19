d4089c515b80f0173f6bef68baa630be8f450cd4
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-10-19 15:34:00 -0300

    conteudo sobre tipos adicionado

--- aula10.19/tipos02.py
+++ aula10.19/tipos02.py
@@ -1,2 +1,13 @@
 # entender tipos e mudanças dentro do python.
 
+a = 50
+b = '50'
+
+print(type(a))
+print(type(b))
+
+float (a = 50)
+float (b = '50')
+# utilizamos do float: moedas, peso, temperatura, medidas e outros
+print(type(a))
+print(type(b))
