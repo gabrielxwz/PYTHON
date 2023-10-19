64cfe8391f1d0b95ace84e1b018204d0721ab953
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-10-19 15:02:29 -0300

    adicionando f_strings.py

---
+++ aula10.18/problema.py
@@ -0,0 +1,5 @@
+nota01 = input("Digite a nota: ")
+nota02 = input("Digite a nota: ")
+nota03 = input("Digite a nota: ")
+nota04 = input("Digite a nota: ")
+print(int(nota01) + int(nota02) + int(nota03) + int(nota04) / 3)
