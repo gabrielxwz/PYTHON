33ab4371655df80f67ccb491376a3dc92b55723e
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-10-31 16:35:19 -0300

    exercicio10.30.py

---
+++ aula10.19/aula10.31/exercicio01.py
@@ -0,0 +1,19 @@
+# aula 10.30 condicionais e repeticao
+# estruturas de repetição
+# comando while
+m = 1 
+while m<4: 
+    print(m)
+    m += 1
+
+# comando for adequado em situações em que o numero de repetições e conhecido
+
+ #                 condição
+# for variaveis (in) expressões:
+    Bloco
+else: #Opcional
+    bloco
+
+# exemplo: faça um ptograma que escreve os numeros inteiros de 1 a 5:
+# for i in ranger(5): #começa em 0
+#   print(i)
