9e1f757e811a05302e6f56a67e0e7e1986f18e0c
levi76325 <145791850+levi76325@users.noreply.github.com>
2023-11-09 16:06:59 -0300

    aula11.09.py adicionada

---
+++ aula11.06/ambiente_virtual/aula11.09.py
@@ -0,0 +1,18 @@
+# QUESTÃO 1 - faça o fatiamento de 10 palavras do texto abaixo.
+# "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, 
+# conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado 
+# compartilhado entre participantes, instrutores e empresas parceiras."
+
+texto = "Somos uma escola de tecnologiade informação que cria pontes entre pessoas, Conhecimento e empresas. Ambiente que proporciona conexão, Troca de conhecimento e aprendizado compartilhado entre participantes, Instrutores e empresas parceiras"
+print(texto[113:124])
+print(texto[10:16])
+print(texto[-9:])
+print(texto[-18:-10])
+print(texto[20:30])
+print(texto[33:44])
+print(texto[53:60])
+print(texto[66:73])
+print(texto[75:88])
+print(texto[-67:-54])
+
+# QUESTÃO 2 - faça o fatiamento de 8 palavras monte uma frase com a impressão dessas palavras.
