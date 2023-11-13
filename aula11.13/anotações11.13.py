c79271d0ea512928c8ad74bf4dbea4ea12792283
gabrielxwz <145721929+gabrielxwz@users.noreply.github.com>
2023-11-13 16:39:56 -0300

    arquivos de aula criados.

---
+++ aula11.13/anotações11.13.py
@@ -0,0 +1,4 @@
+# list - lista = caracteristicas: mutavel ou iteravel, suporta varios tipos de dados, metodos e fatiamentos e tambem possui indices.
+# tuple - tupla
+# set - conjunto
+# dictionary - dicionário
---
+++ aula11.13/lista.py
@@ -0,0 +1,13 @@
+lista[1] = ['10', '20', '30', '40', '50']
+print(lista)
+lista.append ('60')
+[1][2])
+print(lista)
+del lista[-2]
+print(lista)
+del lista[-2]
+print(lista)
+del lista[-2]
+print(lista)
+lista.remove('back')
+print(lista)
