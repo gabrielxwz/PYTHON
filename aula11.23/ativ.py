5f028f1596bd3408fb33214129569bec75cc7258
gabrielxwz <145721929+gabrielxwz@users.noreply.github.com>
2023-11-23 15:57:28 -0300

    commit

---
+++ aula11.23/ativ.py
@@ -0,0 +1,36 @@
+'''
+string = str(input('Digite: '))
+print(string)
+'''
+'''
+string0 = str(input('Digite: '))
+comprimento = len(string0)
+print(comprimento)
+'''
+'''
+nome = str(input('Digite: '))
+QUATROletras = nome[:4]
+print (QUATROletras)
+'''
+'''''
+for i in range (2):
+    str(input('Digite: '))
+if i == i:
+    print ("as strings são iguais")
+else:
+    print("as strings são diferentes")
+'''
+'''
+nome0 = str(input('Digite: '))
+print (nome0.count ('a', 'e', 'i', 'o', 'u'))
+'''
+'''
+usuario =(input("digite seu nome de usúario: "))
+a, e, i,
+print(usuario)
+'''
+
+while True:
+    palavra =  str(input("digite a palavra: "))
+    palavrainvertida = palavra[::-1]
+    print (palavrainvertida)
--- aula11.23/str.py
+++ aula11.23/str.py
@@ -1,8 +1,18 @@
-frase = " a banana é amarela e o abacate é verde"
-letra = 'a'
+# count - função é contar quantas vezes um determinado caractere aparece em uma string.
+# power e lower - dois metodos que deixam todas as strings em minúscuculas ou maiúsculas.
+# find - busca por uma expressão por dentro da frase.
+#replace - é utilizado para realizar alterações dentro das strings.
+# capitalize - deixa a ´primeira letra das palavras maiúsculas
+# split - transforma sua string em uma lista
 
-print (f' A letra { letra } aparece {frase.count(letra)} vezes na frase "{ frase } ".' )
-
-saida = input('Digite "S" para sair: ').upper()
-if saida == 'S':
-    print (saida)
+frase = " A banana é amarela e o abacate é verde.".lower()
+letra = 'e'
+email = ' paulo.junior@gmail.com '
+print(f'A letra "{ letra }" aparece { frase.count(letra)} vezes na frase" { frase }" .')
+achei = frase.find('é')
+if achei >= 0:
+    print(f'A expressão foi encontrada no indice { achei }')
+else:
+    print(f'A expressão NÃO foi encontrada')    
+'''
+saida
